e66957721874407b220a1bcf009ab141f0f8e63c
Agustin Mendez (Menduz) <lord.yo.wo@gmail.com>
2015-07-16 10:50:55 -0300

    Relu and softplus scuashing added

--- src/squash.ts
+++ src/squash.ts
@@ -27,6 +27,18 @@ export function RELU(x: number, derivate?: boolean) {
 	return x <= 0 ? 0 : x;
 }
 
+export function LEAKY_RELU(x: number, derivate?: boolean) {
+	if (derivate)
+		return x <= 0 ? 0 : 1;
+	return x <= 0 ? 0.01 * x : x;
+}
+
+export function SOFTPLUS(x: number, derivate?: boolean) {
+	if (derivate)
+		return 1 / (1 + Math.exp(-x));
+	return Math.log(1 + Math.exp(x));
+}
+
 export function HLIM(x: number, derivate?: boolean) {
 	return derivate ? 1 : +(x > 0);
 }
