7e333ca6bb6c7b6f6ad469c1ffad5d9e3acb69cb
Bertola lucas <lucasbertola@gmail.com>
2016-03-26 01:05:04 +0100

    repair activate

--- dist/synaptic.js
+++ dist/synaptic.js
@@ -1181,15 +1181,30 @@ Neuron.prototype = {
 
     // old state
     this.old = this.state;
-
     // eq. 15
-    this.state = this.selfconnection.gain * this.selfconnection.weight *
-      this.state + this.bias;
+    this.state = this.selfconnection.gain * (this.selfconnection.weight * this.state) + this.bias;
+
 
     for (var i in this.connections.inputs) {
       var input = this.connections.inputs[i];
-      this.state += input.from.activation * input.weight * input.gain;
+      if(input.gater){
+        this.state +=  (input.from.activation * input.weight * input.gain ) ;
+      }
+      
     }
+
+     for (var i in this.connections.inputs) {
+      var input = this.connections.inputs[i];
+      if(!input.gater){
+        this.state +=  ( input.from.activation * input.weight ) ;
+      }
+      
+    }
+
+
+
+
+
 
     // eq. 16
     this.activation = this.squash(this.state);
@@ -1520,7 +1535,12 @@ Neuron.prototype = {
         else
           sentence += 'F[' + args[i].id + ']';
 
+
+
       store.push(sentence + ';');
+
+
+
     }
 
     // helper to check if an object is empty
@@ -1537,7 +1557,6 @@ Neuron.prototype = {
     var noGates = isEmpty(this.connections.gated);
     var isInput = layer == 'input' ? true : isEmpty(this.connections.inputs);
     var isOutput = layer == 'output' ? true : noProjections && noGates;
-
     // optimize neuron's behaviour
     var rate = getVar('rate');
     var activation = getVar(this, 'activation');
@@ -1556,27 +1575,44 @@ Neuron.prototype = {
         var self_weight = getVar(this.selfconnection, 'weight');
       buildSentence(old, ' = ', state, store_activation);
       if (this.selfconnected())
-        if (this.selfconnection.gater)
+        if (this.selfconnection.gater){
           buildSentence(state, ' = ', self_gain, ' * ', self_weight, ' * ',
             state, ' + ', bias, store_activation);
-        else
+         
+        }
+        else {
           buildSentence(state, ' = ', self_weight, ' * ', state, ' + ',
             bias, store_activation);
+        }
+          
       else
         buildSentence(state, ' = ', bias, store_activation);
+      
       for (var i in this.connections.inputs) {
         var input = this.connections.inputs[i];
         var input_activation = getVar(input.from, 'activation');
         var input_weight = getVar(input, 'weight');
-        if (input.gater)
+        if (input.gater){
           var input_gain = getVar(input, 'gain');
-        if (this.connections.inputs[i].gater)
+
+
           buildSentence(state, ' += ', input_activation, ' * ',
             input_weight, ' * ', input_gain, store_activation);
-        else
+
+        }
+          
+        else{
           buildSentence(state, ' += ', input_activation, ' * ',
             input_weight, store_activation);
+
+        }
+          
+
+      
+         
       }
+    
+      
       var derivative = getVar(this, 'derivative');
       switch (this.squash) {
         case Neuron.squash.LOGISTIC:
@@ -1604,7 +1640,7 @@ Neuron.prototype = {
           buildSentence(activation, ' = ', state, ' > 0 ? ', state, ' : 0', store_activation);
           buildSentence(derivative, ' = ', state, ' > 0 ? 1 : 0', store_activation);
           break;
-      }
+      } 
 
       for (var id in this.trace.extended) {
         // calculate extended elegibility traces in advance
@@ -1633,8 +1669,8 @@ Neuron.prototype = {
           }
         }
       }
-
       for (var i in this.connections.inputs) {
+
         var input = this.connections.inputs[i];
         if (input.gater)
           var input_gain = getVar(input, 'gain');
@@ -1643,10 +1679,13 @@ Neuron.prototype = {
           .elegibility[input.ID]);
         if (this.selfconnected()) {
           if (this.selfconnection.gater) {
-            if (input.gater)
+            if (input.gater){
               buildSentence(trace, ' = ', self_gain, ' * ', self_weight,
                 ' * ', trace, ' + ', input_gain, ' * ', input_activation,
                 store_trace);
+              
+            }
+              
             else
               buildSentence(trace, ' = ', self_gain, ' * ', self_weight,
                 ' * ', trace, ' + ', input_activation, store_trace);
@@ -1782,7 +1821,6 @@ Neuron.prototype = {
             buildSentence(input_weight, ' += ', rate, ' * ', gradient,
               store_propagation);
           }
-
         } else if (noGates) {
           buildSentence(responsibility, ' = 0', store_propagation);
           for (var id in this.connections.projected) {
@@ -1856,6 +1894,7 @@ Neuron.prototype = {
       buildSentence(bias, ' += ', rate, ' * ', responsibility,
         store_propagation);
     }
+
     return {
       memory: varID,
       neurons: neurons + 1,
