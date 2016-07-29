5a6c487102dd9e8e6035e54119ffc8dd518a1070
Vsevolod Rodionov <vsevolod.rodionov@gmail.com>
2016-07-29 13:59:50 +0300

    bringing karma for browser testing

---
+++ karma.conf.js
@@ -0,0 +1,25 @@
+// Karma configuration
+
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha'],
+    files: [
+      'dist/synaptic.js',
+      'test/[^_]*.js'
+    ],
+    exclude: [
+    ],
+    preprocessors: {
+      'test/*.js': ['webpack'],
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    singleRun: false,
+    concurrency: Infinity,
+    browserNoActivityTimeout: 60000,
+  })
+}
