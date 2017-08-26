0ffb89855939268126c87ef87c93d8d5fec18b7b
Ville Saukkonen <ville@smartly.io>
2017-08-26 16:23:38 +0300

    enable es6

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
-var webpack = require('webpack')
-var license = require('./prebuild.js')
+const webpack = require('webpack');
+const license = require('./prebuild.js');
+const path = require('path');
 
 module.exports = {
   context: __dirname,
   entry: {
-    synaptic: './src/synaptic.js',
-    'synaptic.min': './src/synaptic.js'
+    synaptic: path.resolve(__dirname, './src/synaptic.js'),
+    'synaptic.min': path.resolve(__dirname, './src/synaptic.js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
   },
   output: {
-    path: 'dist',
+    path: path.resolve(__dirname, './dist'),
     filename: '[name].js',
     library: 'synaptic',
     libraryTarget: 'umd'
