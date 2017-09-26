6498d0bbfc35f335cec9d9fb14e3c42e35a692d9
Stuart Watt <stuart@morungos.com>
2017-09-26 15:25:50 -0400

    Fixed a small issue in standalone generation (#264)

--- src/Network.js
+++ src/Network.js
@@ -427,7 +427,7 @@ export default class Network {
     var activation = 'function (input) {\n';
 
     // build inputs
-    for (var i = 0; i < data.inputs; i++)
+    for (var i = 0; i < data.inputs.length; i++)
       activation += 'F[' + data.inputs[i] + '] = input[' + i + '];\n';
 
     // build network activation
